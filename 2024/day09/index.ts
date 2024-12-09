5f3b14819911033c38a41f638a257dd9d4878d24
MattBidewell <matthewbidewell@gmail.com>
2024-12-09 09:38:56 +0000

    d9 p2

--- 2024/day09/index.ts
+++ 2024/day09/index.ts
@@ -17,18 +17,18 @@ function part1(input: string) {
   let fileId = 0;
 
   for (const [index, letter] of input.split("").entries()) {
-    const dig = parseInt(letter);
+    const digit = parseInt(letter);
 
-    if (isNaN(dig)) {
+    if (isNaN(digit)) {
       // fu line endings
       continue;
     }
 
     if (index % 2 === 0) {
-      disk.push(...Array(dig).fill(fileId));
+      disk.push(...Array(digit).fill(fileId));
       fileId++;
     } else {
-      disk.push(...Array(dig).fill(-1));
+      disk.push(...Array(digit).fill(-1));
     }
   }
 
@@ -52,6 +52,66 @@ function part1(input: string) {
   const result = disk.reduce((sum, x, i) => sum + i * x, 0);
   return result;
 }
-function part2(input: string) {
-  return "";
+
+export function part2(input: string): number {
+  const files: Record<number, [number, number]> = {}; // Map of file ID to [start position, size]
+  let blanks: [number, number][] = []; // List of blanks as [start position, length]
+  let fid = 0;
+  let pos = 0;
+
+  for (const [index, letter] of input.split("").entries()) {
+    const digit = parseInt(letter, 10);
+
+    if (isNaN(digit)) {
+      //fu line endings
+      continue;
+    }
+
+    if (index % 2 === 0) {
+      files[fid] = [pos, digit];
+      fid++;
+    } else {
+      if (digit !== 0) {
+        blanks.push([pos, digit]);
+      }
+    }
+
+    pos += digit;
+  }
+
+  while (fid > 0) {
+    fid--;
+    const [filePos, fileSize] = files[fid];
+
+    for (let i = 0; i < blanks.length; i++) {
+      const [blankStart, blankLength] = blanks[i];
+
+      if (blankStart >= filePos) {
+        // Remove blanks that occur after the file position
+        blanks = blanks.slice(0, i);
+        break;
+      }
+
+      if (fileSize <= blankLength) {
+        files[fid] = [blankStart, fileSize];
+
+        if (fileSize === blankLength) {
+          blanks.splice(i, 1);
+        } else {
+          blanks[i] = [blankStart + fileSize, blankLength - fileSize];
+        }
+        break;
+      }
+    }
+  }
+
+  let total = 0;
+  for (const fid in files) {
+    const [filePos, fileSize] = files[parseInt(fid, 10)];
+    for (let x = filePos; x < filePos + fileSize; x++) {
+      total += parseInt(fid, 10) * x;
+    }
+  }
+
+  return total;
 }
