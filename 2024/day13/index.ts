f56250a53a80fe664769a3d982b8951a3a31d107
MattBidewell <matthewbidewell@gmail.com>
2024-12-13 10:17:56 +0000

    d13-p1

---
+++ 2024/day13/index.ts
@@ -0,0 +1,51 @@
+import { getInput } from "../support";
+
+export function run() {
+  const testInput = getInput(import.meta.url, "input1");
+  const input = getInput(import.meta.url, "input2");
+
+  for (const data of [testInput, input]) {
+    console.log("---");
+    console.time("part1");
+    console.log(part1(data));
+    console.timeEnd("part1");
+    console.time("part2");
+    console.log(part2(data));
+    console.timeEnd("part2");
+    console.log("---");
+  }
+}
+
+function part1(input: string) {
+  const groups = input.trim().split("\n\n");
+  let total = 0;
+
+  for (const group of groups) {
+    const matches = group.match(/\d+/g)?.map(Number);
+
+    if (!matches) continue;
+
+    const [ax, ay, bx, by, px, py] = matches;
+
+    const denominator = ax * by - ay * bx;
+    if (denominator === 0) continue;
+
+    const ca = (px * by - py * bx) / denominator;
+    const cb = (px - ax * ca) / bx;
+
+    if (
+      Number.isInteger(ca) &&
+      Number.isInteger(cb) &&
+      ca <= 100 &&
+      cb <= 100
+    ) {
+      total += ca * 3 + cb;
+    }
+  }
+
+  return total;
+}
+
+function part2(input: string) {
+  return "";
+}
